a8ea66435ba331b8e03dd3825d8978ef9d32a41d
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-03 09:02:41 -0800

    removing the os check for now

--- cookbooks/googlechrome/recipes/default.rb
+++ cookbooks/googlechrome/recipes/default.rb
@@ -1,14 +1,7 @@
 directory "#{ENV['HOME']}/Applications"
 
-case node['platform_family']
-when "mac_os_x"
-	dmg_package "Google Chrome" do
-		destination "#{ENV['HOME']}/Applications"
-		source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
-		action :install
-	end
-else
-	package "chrome" do
-		action :install
-	end
+dmg_package "Google Chrome" do
+	destination "#{ENV['HOME']}/Applications"
+	source "https://dl-ssl.google.com/chrome/mac/stable/GGRM/googlechrome.dmg"
+	action :install
 end
