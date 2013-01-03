a59a3f5d969d01dcfbc612c863ec068f1b4dd298
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-03 09:47:05 -0800

    updating for mac os x home ~/bin

--- cookbooks/chefme/recipes/default.rb
+++ cookbooks/chefme/recipes/default.rb
@@ -1,6 +1,19 @@
-cookbook_file "/usr/local/bin/chefme.sh" do
-	source "chefme.sh"
-	mode 0755
-	owner "root"
-	group "root"
+directory "#{ENV['HOME']}/Applications"
+
+case node['platform_family']
+when "mac_os_x"
+	cookbook_file "~/bin/chefme.sh" do
+		source "chefme.sh"
+		mode 0755
+		owner "root"
+		group "root"
+	end
+		
+else
+	cookbook_file "/usr/local/bin/chefme.sh" do
+		source "chefme.sh"
+		mode 0755
+		owner "root"
+		group "root"
+	end
 end
