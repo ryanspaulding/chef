ed5383e5fd97b783b74e22c9a6764d54c75defdd
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-03 13:59:41 -0800

    adding some conditions to the userprefs

--- cookbooks/userprefs/recipes/default.rb
+++ cookbooks/userprefs/recipes/default.rb
@@ -2,4 +2,5 @@
 
 execute "set_window_double_click" do
 	command "defaults write #{ENV['HOME']}/Library/Preferences/.GlobalPreferences.plist AppleMiniaturizeOnDoubleClick -bool TRUE"
+	not_if "defaults read ~/Library/Preferences/.GlobalPreferences.plist AppleMiniaturizeOnDoubleClick | grep -q 1"
 end
