fb67d0c784de54425f95f0ae9daeb9e1a97604f8
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-03 08:59:12 -0800

    adding google chrome

---
+++ cookbooks/googlechrome/metadata.rb
@@ -0,0 +1,7 @@
+name		 "googlechrome"
+maintainer       "Ryan Spaulding"
+maintainer_email "ryanspaulding@gmail.com"
+license          "BSD"
+description      "Install Google Chrome"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
