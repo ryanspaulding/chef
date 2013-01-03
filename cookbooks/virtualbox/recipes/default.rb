2b4d507745ccd08cdbd138abca2339aec1fe7982
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-02 16:23:18 -0800

    adding in osx support for virtualbox installs

--- cookbooks/virtualbox/recipes/default.rb
+++ cookbooks/virtualbox/recipes/default.rb
@@ -1,3 +1,14 @@
-package "virtualbox" do
-	action :install
+directory "#{ENV['HOME']}/Applications"
+
+case node['platform_family']
+when "mac_os_x"
+	dmg_package "virtualbox-osx" do
+		destination "#{ENV['HOME']}/Applications"
+		source "http://download.virtualbox.org/virtualbox/4.2.6/VirtualBox-4.2.6-82870-OSX.dmg"
+		type "mpkg"
+	end
+else
+	package "virtualbox" do
+		action :install
+	end
 end
