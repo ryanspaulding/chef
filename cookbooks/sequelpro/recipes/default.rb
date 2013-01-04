b191322db80c7798307f14edfcabd95abc6d9e78
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-04 15:42:19 -0800

    had the wrong package name :/

--- cookbooks/sequelpro/recipes/default.rb
+++ cookbooks/sequelpro/recipes/default.rb
@@ -2,7 +2,7 @@ directory "#{ENV['HOME']}/Applications"
 
 # note when you open Sequel Pro the first time it will want to patch itself
 
-dmg_package "Firefox" do
+dmg_package "Sequel Pro" do
 	destination "#{ENV['HOME']}/Applications"
   	source "http://sequel-pro.googlecode.com/files/Sequel_Pro_0.9.9.1.dmg"
 	volumes_dir "Sequel Pro 0.9.9.1"
