adce08e7870e1b378b4d20bfe8d49c51c1ce82dd
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-04 15:40:18 -0800

    adding sequel pro

---
+++ cookbooks/sequelpro/metadata.rb
@@ -0,0 +1,7 @@
+name		 "sequelpro"
+maintainer       "Ryan Spaulding"
+maintainer_email "ryanspaulding@gmail.com"
+license          "BSD"
+description      "Install Sequel Pro"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/sequelpro/recipes/default.rb
@@ -0,0 +1,10 @@
+directory "#{ENV['HOME']}/Applications"
+
+# note when you open Sequel Pro the first time it will want to patch itself
+
+dmg_package "Firefox" do
+	destination "#{ENV['HOME']}/Applications"
+  	source "http://sequel-pro.googlecode.com/files/Sequel_Pro_0.9.9.1.dmg"
+	volumes_dir "Sequel Pro 0.9.9.1"
+	action :install
+end
