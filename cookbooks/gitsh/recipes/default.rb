1752dcd5c82dd157cae229e2e33f8da7cfef9bd0
Ryan Spaulding <ryanspaulding@gmail.com>
2013-01-03 16:03:50 -0800

    typo :(

--- cookbooks/gitsh/recipes/default.rb
+++ cookbooks/gitsh/recipes/default.rb
@@ -1,5 +1,5 @@
 
 execute "install git-sh" do
-	command "~/homebrew/bin/brew instsall git-sh" 
+	command "~/homebrew/bin/brew install git-sh" 
 	not_if "~/homebrew/bin/brew list | grep -q git-sh"
 end
